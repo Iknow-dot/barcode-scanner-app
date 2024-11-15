3c2dac00e7bab884460fc93129751ba2a8505688
ninitapliashvili <84851232+ninitapliashvili@users.noreply.github.com>
2024-11-14 18:33:21 -0800

    shota- save-is suratebi da back is linki

--- barcode-scanner-frontend/src/api.js
+++ barcode-scanner-frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set up the base URL for the API
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://iflow.ge:4433',
   headers: {
     'Content-Type': 'application/json',
   },
