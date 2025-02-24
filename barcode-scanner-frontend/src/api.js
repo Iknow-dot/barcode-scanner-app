1467c1d6078e7bc478e3517cc4e0f2c9e6ccb050
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-02-24 13:07:37 +0400

    Add debugging step for env

--- barcode-scanner-frontend/src/api.js
+++ barcode-scanner-frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 
+console.debug(process.env);
 // Set up the base URL for the API
 const api = axios.create({
   baseURL: process.env.API_BASE_URL || 'http://localhost:5000',
