cb0a3bf19057fb22c5ea33d197c1864ac4538f47
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-02-24 13:51:31 +0400

    Add stream handler for logging at DEBUG level

--- config.py
+++ config.py
@@ -42,8 +42,10 @@ class Config:
                 '[in %(pathname)s:%(lineno)d]'
             ))
             file_handler.setLevel(logging.INFO)
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.DEBUG)
+            app.logger.addHandler(stream_handler)
             app.logger.addHandler(file_handler)
-            app.logger.setLevel(logging.INFO)
             app.logger.info('Application startup')
 
 class DevelopmentConfig(Config):
