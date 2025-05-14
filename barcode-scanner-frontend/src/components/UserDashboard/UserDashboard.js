300f0558fed72f80ac8e3441cff9f5e5e52646f7
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-05-14 16:19:06 +0400

    Enhance UserDashboard; close product search drawer upon successful search

--- barcode-scanner-frontend/src/components/UserDashboard/UserDashboard.js
+++ barcode-scanner-frontend/src/components/UserDashboard/UserDashboard.js
@@ -89,6 +89,7 @@ const UserDashboard = () => {
           price: data.price,
           img_url: data.images
         });
+        setDrawerVisible(false);
       } else {
         setBalances([]);
         setProductInfo({sku_name: '', article: '', price: '', img_url: []});
@@ -157,183 +158,183 @@ const UserDashboard = () => {
 
             </Result>
         )}
-        <Button
-            type="primary"
-            style={{
-              position: 'fixed',
-              bottom: 50,
-              right: "50%",
-              transform: 'translateX(50%)',
-              border: 'none',
-              zIndex: 1000,
-            }}
-            onClick={() => setDrawerVisible(prev => !prev)}>
-          <UpOutlined/>
-        </Button>
-        <Spin
-            spinning={loading}
-            tip="ვეძებ პროდუქტს..."
-            style={{
-              background: 'rgba(0, 0, 0, 0.1)',
-              borderRadius: 4,
-            }}
-            size="large"
-        >
-          <Drawer
-              title="პროდუქტის ძიება"
-              placement="bottom"
-              closable={true}
-              open={drawerVisible}
-              onClose={() => setDrawerVisible(false)}
+          <Button
+              type="primary"
+              style={{
+                position: 'fixed',
+                bottom: 50,
+                right: "50%",
+                transform: 'translateX(50%)',
+                border: 'none',
+                zIndex: 1000,
+              }}
+              onClick={() => setDrawerVisible(prev => !prev)}>
+            <UpOutlined/>
+          </Button>
+          <Spin
+              spinning={loading}
+              tip="ვეძებ პროდუქტს..."
+              style={{
+                background: 'rgba(0, 0, 0, 0.1)',
+                borderRadius: 4,
+              }}
+              size="large"
           >
-            <Form
-                form={form}
-                onFinish={handleSearch}
-                initialValues={{
-                  searchType: 'barcode'
-                }}
-                layout="horizontal"
+            <Drawer
+                title="პროდუქტის ძიება"
+                placement="bottom"
+                closable={true}
+                open={drawerVisible}
+                onClose={() => setDrawerVisible(false)}
             >
-              <Flex
-                  gap="middle"
-                  justify="center"
-              >
-                <Form.Item
-                    name="searchType"
-                    initialValue="barcode"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'გთხოვთ აირჩიოთ ძიების ტიპი!',
-                      },
-                    ]}
-                >
-                  <Select
-                      options={[
-                        {
-                          label: (
-                              <>
-                                <BarcodeOutlined/> შტრიხკოდი
-                              </>
-                          ),
-                          value: "barcode",
-                        },
-                        {
-                          label: (
-                              <>
-                                <NumberOutlined/> არტიკული
-                              </>
-                          ),
-                          value: "article",
-                        }
-                      ]}
-                      onChange={(value) => {
-                        if (value === 'barcode') {
-                          setBalances([]);
-                          setDisableScan(false);
-                        } else {
-                          setDisableScan(true);
-                        }
-                      }}
-                  />
-                </Form.Item>
-                <Form.Item
-                    name="search"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'გთხოვთ შეიყვანოთ ძიების ტექსტი!',
-                      },
-                    ]}
-                >
-                  <Input.Search
-                      placeholder="ძიება"
-                      enterButton={<SearchOutlined/>}
-                      onSearch={form.submit}
-                      allowClear
-                  />
-                </Form.Item>
-              </Flex>
-              <Flex
-                  gap="middle"
-                  justify="center"
-              >
-                <Form.Item
-                    name="allWarehouses"
-                    label="ყველა საწყობი"
-                    initialValue={false}
-
-                >
-                  <Switch/>
-                </Form.Item>
-              </Flex>
-
-            </Form>
-            <ScanButton
-                setScanning={() => {
-                  setScanning(prev => !prev);
-                  setDrawerVisible(false);
-                }}
-                scanning={scanning}
-                onScan={handleScanResult}
-                disabled={disableScan}
-                qrRef={qrRef}
-            />
-
-          </Drawer>
-
-        </Spin>
-        {!scanning && balances.length > 0 && (
-            <>
-              <Descriptions>
-                <Descriptions.Item label="პროდუქტი">
-                  {productInfo.sku_name}
-                </Descriptions.Item>
-                <Descriptions.Item label="არტიკული">{productInfo.article}</Descriptions.Item>
-              </Descriptions>
-              <Carousel
-                  arrows
-                  infinite
-                  style={{
-                    margin: '0 auto',
-                    width: '300px',
+              <Form
+                  form={form}
+                  onFinish={handleSearch}
+                  initialValues={{
+                    searchType: 'barcode'
                   }}
+                  layout="horizontal"
               >
-                {productInfo.img_url.map((img, index) => (
-                    <div key={index}>
-                      <img
-                          src={img}
-                          style={{width: '100%'}}
-                      />
-                    </div>
-                ))}
-              </Carousel>
-              <Table
-                  dataSource={balances}
-                  rowClassName={(record, index) => userWarehouses.map(wh => wh.name).includes(record.warehouse_name) ? 'highlight-row' : ''}
-                  columns={[
-                    {
-                      title: 'საწყობი',
-                      dataIndex: 'warehouse_name',
-                      key: 'warehouse_name'
-                    },
-                    {
-                      title: 'ნაშთი',
-                      dataIndex: 'quantity',
-                      key: 'quantity'
-                    },
-                    {
-                      title: 'ფასი',
-                      dataIndex: 'price',
-                      key: 'price',
-                      render: () => (
-                          <span>{productInfo.price} ₾</span>
-                      )
-                    }
-                  ]}
+                <Flex
+                    gap="middle"
+                    justify="center"
+                >
+                  <Form.Item
+                      name="searchType"
+                      initialValue="barcode"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'გთხოვთ აირჩიოთ ძიების ტიპი!',
+                        },
+                      ]}
+                  >
+                    <Select
+                        options={[
+                          {
+                            label: (
+                                <>
+                                  <BarcodeOutlined/> შტრიხკოდი
+                                </>
+                            ),
+                            value: "barcode",
+                          },
+                          {
+                            label: (
+                                <>
+                                  <NumberOutlined/> არტიკული
+                                </>
+                            ),
+                            value: "article",
+                          }
+                        ]}
+                        onChange={(value) => {
+                          if (value === 'barcode') {
+                            setBalances([]);
+                            setDisableScan(false);
+                          } else {
+                            setDisableScan(true);
+                          }
+                        }}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                      name="search"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'გთხოვთ შეიყვანოთ ძიების ტექსტი!',
+                        },
+                      ]}
+                  >
+                    <Input.Search
+                        placeholder="ძიება"
+                        enterButton={<SearchOutlined/>}
+                        onSearch={form.submit}
+                        allowClear
+                    />
+                  </Form.Item>
+                </Flex>
+                <Flex
+                    gap="middle"
+                    justify="center"
+                >
+                  <Form.Item
+                      name="allWarehouses"
+                      label="ყველა საწყობი"
+                      initialValue={false}
+
+                  >
+                    <Switch/>
+                  </Form.Item>
+                </Flex>
+
+              </Form>
+              <ScanButton
+                  setScanning={() => {
+                    setScanning(prev => !prev);
+                    setDrawerVisible(false);
+                  }}
+                  scanning={scanning}
+                  onScan={handleScanResult}
+                  disabled={disableScan}
+                  qrRef={qrRef}
               />
-            </>
-        )}
+
+            </Drawer>
+
+          </Spin>
+          {!scanning && balances.length > 0 && (
+              <>
+                <Descriptions>
+                  <Descriptions.Item label="პროდუქტი">
+                    {productInfo.sku_name}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="არტიკული">{productInfo.article}</Descriptions.Item>
+                </Descriptions>
+                <Carousel
+                    arrows
+                    infinite
+                    style={{
+                      margin: '0 auto',
+                      width: '300px',
+                    }}
+                >
+                  {productInfo.img_url.map((img, index) => (
+                      <div key={index}>
+                        <img
+                            src={img}
+                            style={{width: '100%'}}
+                        />
+                      </div>
+                  ))}
+                </Carousel>
+                <Table
+                    dataSource={balances}
+                    rowClassName={(record, index) => userWarehouses.map(wh => wh.name).includes(record.warehouse_name) ? 'highlight-row' : ''}
+                    columns={[
+                      {
+                        title: 'საწყობი',
+                        dataIndex: 'warehouse_name',
+                        key: 'warehouse_name'
+                      },
+                      {
+                        title: 'ნაშთი',
+                        dataIndex: 'quantity',
+                        key: 'quantity'
+                      },
+                      {
+                        title: 'ფასი',
+                        dataIndex: 'price',
+                        key: 'price',
+                        render: () => (
+                            <span>{productInfo.price} ₾</span>
+                        )
+                      }
+                    ]}
+                />
+              </>
+          )}
       </>
   );
 };
