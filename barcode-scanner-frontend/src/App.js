be487f3f4fb98bca072129343454d3f1bc8b9305
lukatavkhelidze <tavkhelidzeluka@gmail.com>
2026-01-12 11:03:15 +0100

    Replace Link components with anchor tags for logo navigation in App.js

--- barcode-scanner-frontend/src/App.js
+++ barcode-scanner-frontend/src/App.js
@@ -69,7 +69,7 @@ const MainContentView = ({children}) => {
                     collapsible={!!subNav}
                     collapsed={!subNav}
                 >
-                    <Link to={window.location.host}>
+                    <a href={window.location.host}>
                         <img
                             src={isDarkMode ? "logo-dark.png" : "logo-light.png"}
                             alt="Logo"
@@ -79,7 +79,7 @@ const MainContentView = ({children}) => {
                                 display: "block",
                             }}
                         />
-                    </Link>
+                    </a>
                     <Menu theme={isDarkMode ? "dark" : "light"}
                           mode="inline"
                           defaultSelectedKeys={authData?.role === "system_admin" ? ['1'] : ['2']}
@@ -91,13 +91,13 @@ const MainContentView = ({children}) => {
                 <Header style={{padding: "0 24px", background: colorBgContainer}}>
                     {!screens.lg && (
                         <Flex>
-                            <Link to={window.location.host}>
+                            <a href={window.location.host}>
                                 <img
                                     src={isDarkMode ? "logo-dark.png" : "logo-light.png"}
                                     alt="Logo"
                                     width="75px"
                                 />
-                            </Link>
+                            </a>
                             <Menu
                                 style={{
                                     flex: 1,
